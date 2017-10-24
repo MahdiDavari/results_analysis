eccf3fef2fc4d944a84f148e73006cb61f016513
Mahdi Davari <mahdi.davari@stonybrook.edu>
2017-10-24 12:40:15 -0400

    Create PARSE_CONVEX_HULL.py
    File created

---
+++ PARSE_CONVEX_HULL.py
@@ -0,0 +1,38 @@
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='''
+ACS Programming Challenge |
+Author: Mahdi Davari |
+Last updated: August, 2017''')
+
+
+parser.add_argument('-f', metavar='files', type=str, nargs=1,
+                    help='Input file for files, e.g. -f files.txt. Required')
+parser.add_argument('-n', metavar='nodes', type=str, nargs=1,
+                    help='Input file for nodes, e.g. -n nodes.txt. Required')
+parser.add_argument('-o', metavar='result', type=str, nargs=1,
+                    help='Output file, e.g. -o result.txt. Default: standard output')
+
+
+args = parser.parse_args()
+if args.f and args.n:
+    file_key = args.f[0]
+    file_extract = args.n[0]
+else:
+    sys.stderr.write('You should provide input files for file_name and node_names, i.e., -f and -n. For more information, use -h')
+    sys.exit(1)
+
+
+
+def msg(file_key,file_extract):
+    a = 0
+    with open(file_key) as file:
+        for lines in file:
+            lines = lines.split()
+            a=int(lines[2])+int(lines[3])+int(7.0)
+            keyword = 'EA'+str(lines[0])
+            subprocess.call(["/usr/bin/grep","-A",str(a), str(keyword),file_extract])
+            return TRUE
+
+msg('extended_convex_hull', 'GPa')
